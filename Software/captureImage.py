f5395d92ce597bb7c25ec21542663b06207ee738
Viljami Pirttimaa <viljami.pirttimaa@gmail.com>
2018-02-07 21:37:37 +0200

    Paranneltu uutta nopantunnistusta
    Lisätty tunnistus pisteiden etäisyydelle keskiarvosta
    Lisätty tunnistus pisteiden hierarkiasta

--- Software/captureImage.py
+++ Software/captureImage.py
@@ -2,8 +2,8 @@ import sys
 import cv2 as cv
 
 # Debuggauksen ajaksi kuva luetaan tiedostosta, oikeasti siirrytään käyttämään videoCapturea joka käyttää kameraa
-#vcap = cv2.VideoCapture(1)
-vcap = cv.imread('img/kimble2.jpg')
+#vcap = cv.VideoCapture(1)
+vcap = cv.imread('img/kimble8.jpg')
 
 def captureFrame():
     #ret, frame = vcap.read()
--- Software/detectDie.py
+++ Software/detectDie.py
@@ -1,45 +1,81 @@
 import cv2 as cv
 import numpy as np
+import math
 
-BINARIZATION_THRESHOLD = 50
-MIN_PIP_FACTOR = 0.6
-MAX_PIP_FACTOR = 1.4
+MIN_AREA = 5 
+MAX_AREA = 45
+MAX_CIRCULARITY_DEVIATION = 0.36
+TRESHOLD = 120
+MAX_DISTANCE_FROM_CENTER = 28
+DIAMETER = 180
 
-erodeKernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(6,6))
-dilateKernel = cv.getStructuringElement(cv.MORPH_CROSS,(2,2))
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(2,2))
 
 def detect(frame):
-    diameter = 170
     width = frame.shape[0]
     height = frame.shape[1]
-    roi = frame[(int(height/ 2) - int(diameter / 2)):(int(height / 2) + int(diameter / 2)), (int(width / 2) - int(diameter / 2)):(int(width / 2) + int(diameter / 2))]
-    gray = cv.cvtColor(roi, cv.COLOR_RGB2GRAY) 
-    retval, bin = cv.threshold(gray, BINARIZATION_THRESHOLD, 255, cv.THRESH_BINARY) # select white die areas
-    bin = cv.dilate(bin,dilateKernel, iterations=1) # dilate white areas to prevent pip fraying
-    bin = cv.erode(bin,erodeKernel, iterations=1) # dilate white areas to prevent pip fraying 
-    #bin = cv.dilate(bin,dilateKernel, iterations=1) # dilate white areas to prevent pip fraying
-    bin = cv.bitwise_not(bin)
-    mask = np.zeros(bin.shape, dtype = "uint8")
-    cv.circle(mask, (int(mask.shape[0]*0.5), int(mask.shape[1]*0.5)), int(diameter/1.9), (255,255,255),-1)
-    bin = cv.bitwise_and(bin, mask)
-    cv.imshow("Noppa", bin)
-    cv.waitKey(0)
-    binWcontours, contours0, hierarchy = cv.findContours(bin, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # find contours
+    cv.normalize(frame, frame, 0, 255, cv.NORM_MINMAX)
+    roi = frame[(int(height/ 2) - int(DIAMETER / 2)):(int(height / 2) + int(DIAMETER / 2)), (int(width / 2) - int(DIAMETER / 2)):(int(width / 2) + int(DIAMETER / 2))]
+    grey = cv.cvtColor(roi, cv.COLOR_RGB2GRAY) 
+    grey = cv.bilateralFilter(grey, 9, 75, 75)
+    cv.imshow("bilateral", grey)
+    #ret, threshold = cv.threshold(grey, TRESHOLD, 255, cv.THRESH_BINARY_INV)
+    threshold = cv.adaptiveThreshold(grey,255,cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY_INV,11,2)
+    threshold = cv.morphologyEx(threshold, cv.MORPH_CLOSE, kernel, iterations=1)
+    mask = np.zeros(threshold.shape, dtype = "uint8")
+    cv.circle(mask, (int(mask.shape[0]*0.5), int(mask.shape[1]*0.5)), int(DIAMETER*0.48), (255,255,255),-1)
+    mask = cv.bitwise_not(mask)
+    threshold = cv.bitwise_or(threshold, mask)
+    cv.imshow("threshold", threshold)
+    treshWcontours, contours0, hierarchy = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # find contours
     contours = [cv.approxPolyDP(cnt, 2, True) for cnt in contours0] # simplify contours
 
-    pipCount = 0
-    contourAreas = []
+    count = 0
+    centroids = []
+    parents = []
+    print(hierarchy)
 
+    contourId = 0
     for c in contours:
-        pipArea = cv.contourArea(c)
-        print(pipArea)
-        contourAreas.append(pipArea)
-    medianArea = np.median(contourAreas)
-    print("median area: "+ str(medianArea))
+        area = cv.contourArea(c)
+        if (area > MIN_AREA) and (area < MAX_AREA): # Pinta-ala on rajojen sisällä
+            perimeter = cv.arcLength(c, True)
+            r = math.sqrt(area/math.pi)
+            perfectPerimeter = 2 * math.pi *r
+            circularity = (perimeter / perfectPerimeter) - 1
+            if abs(circularity) < MAX_CIRCULARITY_DEVIATION: # Ympyrämäisyys on riittävän hyvä
+                M = cv.moments(c)
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                if threshold[cy][cx] == 255: # Keskipisteen väri on valkoinen
+                    centroids.append((cx,cy))
+                    parentId = hierarchy[0][contourId][3]
+                    parents.append(parentId)
+                    print("Area: " + str(area) + " Perimeter: "+ str(perimeter) + " Circularity: " + str((perimeter / perfectPerimeter)-1) + " x: " + str(cx) + " y: " + str(cy) + " pID: " + str(parentId))
+        contourId += 1
+            # Etsitään tresholdista kaikki contourit ja hylätään liian pienet ja suuret.
+            # Sitten lasketaan ensin r = sqrt(Area/pi) ja vertaa 2*pi*r suuruutta arcLenghtiin, jos niiden suhde on lähellä 1, on alue pyöreä
+            # Jos contourin keskipisteen väri on valkoinen eli 255, on se todennäköisesti nopan piste.
+            # Jos contourin keskipiste on liian kaukana pisteiden keskiarvosta, ei se taida olla piste
+            # Jos contourin parent on eri kuin mediaaniparent, se hylätään
 
-    for c in contours:
-        pipArea = cv.contourArea(c)
-        if ((pipArea < medianArea * MAX_PIP_FACTOR) and (pipArea > medianArea * MIN_PIP_FACTOR)):
-            pipCount += 1
+    mean = tuple(map(lambda y: sum(y) / float(len(y)), zip(*centroids)))
+    print(mean)
+    medianParent = np.median(parents)
+    print("Median parent: " + str(medianParent))
 
-    return pipCount
+    contourId = 0
+    for c in centroids:
+        distance = math.sqrt((mean[0] - c[0])**2 + (mean[1] - c[1])**2)
+        if distance < MAX_DISTANCE_FROM_CENTER:
+            if parents[contourId] == medianParent:
+                count += 1
+        contourId += 1
+
+    if count > 6: # Jos mukaan on tarttunut enemmänkin pisteitä, ei anneta ainakaan yli 6
+        count = 6
+    if count == 0: # Jos mukaan on tarttunut enemmänkin pisteitä, ei anneta ainakaan yli 6
+        count = 1
+
+    return count
+
