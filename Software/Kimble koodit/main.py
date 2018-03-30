e23da2fa7cceefd4a46b70a6c639455f41148449
Teemu Laine <teemumatias.laine@aalto.fi>
2018-03-30 19:29:55 +0300

    input testi

--- Software/Kimble koodit/main.py
+++ Software/Kimble koodit/main.py
@@ -7,11 +7,14 @@ def main():
     hardware.valo(true)
     
     while True:
+        
+        input("Haluatko jatkaa?")
+        '''
         if (nappi == True):
             hardware.valo(false)
             peli.pelaa()
             hardware.valo(true)
-            
+            '''
             #Mahdollinen odotus looppiin, ehka tarpeeton. Aika ilmeisesti sekunteina.
             #time.sleep(1)    
     
