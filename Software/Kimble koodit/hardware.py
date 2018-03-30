34682db64169291668e602aa412bae95b2caf504
Juho Piilola <juho.piilola@aalto.fi>
2018-03-30 13:29:33 +0300

    lisää HW:tä

--- Software/Kimble koodit/hardware.py
+++ Software/Kimble koodit/hardware.py
@@ -16,30 +16,109 @@ def homing():
     print('Sending: $H ')
     s.write('$H') # Send g-code block to grbl
     grbl_out = s.readline() # Wait for grbl response with carriage return
-    print ' : ' + grbl_out.strip()
+    print(' : ' + grbl_out.strip())
 
-# Wait here until grbl is finished to close serial port and file.
-#raw_input("  Press <Enter> to exit and disable grbl.") 
+    # Wait here until grbl is finished to close serial port and file.
+    #raw_input("  Press <Enter> to exit and disable grbl.") 
 
-# Close file and serial port
-f.close()
-s.close()  
+    # Close file and serial port
+    s.close()  
 
     
     return
 
 def kuvaAsento():
+    # Open grbl serial port
+    s = serial.Serial('/dev/tty.usbmodem1811',115200)
+
+    #Wake up grbl
+    s.write("\r\n\r\n")
+    time.sleep(2)   # Wait for grbl to initialize 
+    s.flushInput()  # Flush startup text in serial input
+
+    # Stream g-code to grbl
+    
+    print('Sending: $H ')
+    s.write('$H') # Send g-code block to grbl
+    grbl_out = s.readline() # Wait for grbl response with carriage return
+    print(' : ' + grbl_out.strip())
+
+    # Wait here until grbl is finished to close serial port and file.
+    #raw_input("  Press <Enter> to exit and disable grbl.") 
+
+    # Close file and serial port
+    s.close()  
 
     return
 
 def peliAsento():
+    # Open grbl serial port
+    s = serial.Serial('/dev/tty.usbmodem1811',115200)
+
+    #Wake up grbl
+    s.write("\r\n\r\n")
+    time.sleep(2)   # Wait for grbl to initialize 
+    s.flushInput()  # Flush startup text in serial input
+
+    # Stream g-code to grbl
+    
+    print('Sending: $H ')
+    s.write('$H') # Send g-code block to grbl
+    grbl_out = s.readline() # Wait for grbl response with carriage return
+    print(' : ' + grbl_out.strip())
+
+    # Wait here until grbl is finished to close serial port and file.
+    #raw_input("  Press <Enter> to exit and disable grbl.") 
+
+    # Close file and serial port
+    s.close()  
 
     return
 
 def painaNoppaa():
+    # Open grbl serial port
+    s = serial.Serial('/dev/tty.usbmodem1811',115200)
+
+    #Wake up grbl
+    s.write("\r\n\r\n")
+    time.sleep(2)   # Wait for grbl to initialize 
+    s.flushInput()  # Flush startup text in serial input
+
+    # Stream g-code to grbl
+    
+    print('Sending: $H ')
+    s.write('$H') # Send g-code block to grbl
+    grbl_out = s.readline() # Wait for grbl response with carriage return
+    print(' : ' + grbl_out.strip())
+
+    # Wait here until grbl is finished to close serial port and file.
+    #raw_input("  Press <Enter> to exit and disable grbl.") 
+
+    # Close file and serial port
+    s.close()  
 
     return
 
 def siirra(i1,i2):
+    # Open grbl serial port
+    s = serial.Serial('/dev/tty.usbmodem1811',115200)
+
+    #Wake up grbl
+    s.write("\r\n\r\n")
+    time.sleep(2)   # Wait for grbl to initialize 
+    s.flushInput()  # Flush startup text in serial input
+
+    # Stream g-code to grbl
+    
+    print('Sending: $H ')
+    s.write('$H') # Send g-code block to grbl
+    grbl_out = s.readline() # Wait for grbl response with carriage return
+    print(' : ' + grbl_out.strip())
+
+    # Wait here until grbl is finished to close serial port and file.
+    #raw_input("  Press <Enter> to exit and disable grbl.") 
+
+    # Close file and serial port
+    s.close()  
 
     return
