58c596e810cd3764254bcf5fa86da50b96be7260
Teemu Laine <teemumatias.laine@aalto.fi>
2018-03-30 11:55:48 +0300

    Mainin päivitys

--- Software/Kimble koodit/main.py
+++ Software/Kimble koodit/main.py
@@ -1,1 +1,16 @@
 import hardware
+
+def main():
+    hardware.homing()
+    hardware.kuvaAsento()
+    kamera.kalibroiPerspektiivi()
+    hardware.valo(true)
+    
+    while True:
+        if nappi == True:
+            hardware.valo(false)
+            peli.pelaa()
+            hardware.valo(true)
+    
+    
+    return
