4286f0776996d29b657c73d5dd6fa14a54da106e
Juho Piilola <juho.piilola@aalto.fi>
2018-03-30 17:35:02 +0300

    hardware moduulia räpeletty

--- Software/Kimble koodit/hardware.py
+++ Software/Kimble koodit/hardware.py
@@ -1,49 +1,53 @@
 import serial
 import time
 
+def openSerial():
+    global s
+    s = serial.Serial("COM9",115200) #/dev/cu.usbserial-DN03VXG 
+    return 1
+
+def closeSerial():
+    s.close()
+    
 
 def homing():
     # Open grbl serial port
-    siirto = '$H'
-    s = serial.Serial('/dev/tty.usbmodem1811',115200)
-
+    siirto = '$H\n'
+    string = '$#\n'
     #Wake up grbl
-    s.write("\r\n\r\n")
+    s.write(string.encode('utf-8'))
     time.sleep(2)   # Wait for grbl to initialize 
     s.flushInput()  # Flush startup text in serial input
 
     # Stream g-code to grbl
     
-    print('Sending:', siirto)
-    s.write(siirto) # Send g-code block to grbl
+    print("Sending:", siirto.encode('utf-8'))
+    s.write(siirto.encode()) # Send g-code block to grbl
     grbl_out = s.readline() # Wait for grbl response with carriage return
-    print(' : ' + grbl_out.strip())
+    print(" Vastaus" , grbl_out.strip())
 
     # Wait here until grbl is finished to close serial port and file.
     #raw_input("  Press <Enter> to exit and disable grbl.") 
-
-    # Close file and serial port
-    s.close()  
 
     
     return
 
 def kuvaAsento():
     # Open grbl serial port
-    siirto = 'G90 X0 Y0 Z0' #MUUTA NAMA
-    s = serial.Serial('/dev/tty.usbmodem1811',115200)
-
+    siirto = 'G90 X0 Y0 Z0\n' #MUUTA NAMA
+    s = serial.Serial("COM9",115200)
+    string = '$#\n'
     #Wake up grbl
-    s.write("\r\n\r\n")
+    s.write(string.encode('utf-8'))
     time.sleep(2)   # Wait for grbl to initialize 
     s.flushInput()  # Flush startup text in serial input
 
     # Stream g-code to grbl
     
-    print('Sending:', siirto)
-    s.write(siirto) # Send g-code block to grbl
+    print("Sending:", siirto.encode('utf-8'))
+    s.write(siirto.encode()) # Send g-code block to grbl
     grbl_out = s.readline() # Wait for grbl response with carriage return
-    print(' : ' + grbl_out.strip())
+    print(" Vastaus" , grbl_out.strip())
 
     # Wait here until grbl is finished to close serial port and file.
     #raw_input("  Press <Enter> to exit and disable grbl.") 
@@ -51,24 +55,25 @@ def kuvaAsento():
     # Close file and serial port
     s.close()  
 
+    
     return
 
 def peliAsento():
     # Open grbl serial port
-    siirto = 'G90 X0 Y0 Z0' #MUUTA NAMA
-    s = serial.Serial('/dev/tty.usbmodem1811',115200)
-
+    siirto = 'G90 X0 Y0 Z0\n' #MUUTA NAMA
+    s = serial.Serial("COM9",115200)
+    string = '$#\n'
     #Wake up grbl
-    s.write("\r\n\r\n")
+    s.write(string.encode('utf-8'))
     time.sleep(2)   # Wait for grbl to initialize 
     s.flushInput()  # Flush startup text in serial input
 
     # Stream g-code to grbl
     
-    print('Sending:', siirto)
-    s.write(siirto) # Send g-code block to grbl
+    print("Sending:", siirto.encode('utf-8'))
+    s.write(siirto.encode()) # Send g-code block to grbl
     grbl_out = s.readline() # Wait for grbl response with carriage return
-    print(' : ' + grbl_out.strip())
+    print(" Vastaus" , grbl_out.strip())
 
     # Wait here until grbl is finished to close serial port and file.
     #raw_input("  Press <Enter> to exit and disable grbl.") 
@@ -76,6 +81,7 @@ def peliAsento():
     # Close file and serial port
     s.close()  
 
+    
     return
 
 def painaNoppaa():
@@ -131,3 +137,5 @@ def siirra(i1,i2):
     s.close()  
 
     return
+ 
+homing()
